a59512db855f744c7e93c9382c411e7ad225a979
n-gredlein <85944691+n-gredlein@users.noreply.github.com>
2021-12-22 12:02:51 +0100

    Start and Info Page
    Background, Font...

--- lib/InfoPage.dart
+++ lib/InfoPage.dart
@@ -7,29 +7,43 @@ class InfoPage extends StatelessWidget {
         body: Container(
             decoration: BoxDecoration(
                 image: DecorationImage(
-                    image: const AssetImage("assets/background.png"),
+                    image: const AssetImage("assets/images/background.jpg"),
                     fit: BoxFit.cover)),
             child: Center(
                 child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Container(
-                    width: 80,
-                    child: Row(children: [
-                      Icon(Icons.info_outline),
-                      Text("INFO", style: TextStyle(fontFamily: 'Pixellari'))
-                    ])),
+                    width: 166,
+                    child: Center(
+                        child: Row(children: [
+                      Icon(
+                        Icons.info_outline,
+                        color: Colors.white,
+                        size: 50,
+                      ),
+                      Text("INFO",
+                          style: TextStyle(
+                            fontFamily: 'Pixellari',
+                            fontSize: 50,
+                            color: Colors.white,
+                          ))
+                    ]))),
                 Container(
-                  width: 500,
+                  width: 300,
                   child: Text(
-                      "Du kannst dich im Museum frei bewegen, mit Ausnahme eines Raumes, wo sich ein Schatz befindet. Für diesen Raum musst du dir den Schlüssel erspielen. Dafür gehst du zu den Stationen im Museum. Dort erhältst du Buchstaben. Wenn du bei jeder Station gewesen ist, wirst du zur verschlossenen Tür geführt. Dort muss du die gefundenen Buchstaben in der richtigen Reihenfolge eingeben, um die Tür zu öffnen. Im Raum hinter der Tür befindet sich der Schatz.",
-                      textAlign: TextAlign.center),
+                    "Du kannst dich im Museum frei bewegen, mit Ausnahme eines Raumes, wo sich ein Schatz befindet. Für diesen Raum musst du dir den Schlüssel erspielen. Dafür gehst du zu den Stationen im Museum. Dort erhältst du Buchstaben. Wenn du bei jeder Station gewesen ist, wirst du zur verschlossenen Tür geführt. Dort muss du die gefundenen Buchstaben in der richtigen Reihenfolge eingeben, um die Tür zu öffnen. Im Raum hinter der Tür befindet sich der Schatz.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
                 Container(
-                    width: 127,
+                    width: 128,
                     height: 40,
                     child: ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        Navigator.pop(context, true);
+                      },
                       child: Row(
                         children: [
                           Icon(Icons.check),
--- lib/startPage.dart
+++ lib/startPage.dart
@@ -8,14 +8,23 @@ class StartPage extends StatelessWidget {
         body: Container(
             decoration: BoxDecoration(
                 image: DecorationImage(
-                    image: const AssetImage("assets/background.png"),
+                    image: const AssetImage("assets/images/background.jpg"),
                     fit: BoxFit.cover)),
             child: Center(
                 child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text('Das kleine Gespenst im Museum',
-                    style: TextStyle(fontFamily: 'Pixellari')),
+                Text('DAS KLEINE GESPENST',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: 'Pixellari',
+                        fontSize: 50,
+                        color: Colors.white)),
+                Text('IM MUSEUM',
+                    style: TextStyle(
+                        fontFamily: 'Pixellari',
+                        fontSize: 20,
+                        color: Colors.white)),
                 Image.asset('assets/images/ghost_right.png', scale: 0.2),
                 Container(
                     width: 102,
@@ -34,142 +43,16 @@ class StartPage extends StatelessWidget {
                               MaterialStateProperty.all<Color>(Colors.lime)),
                     )),
                 FloatingActionButton(
-                    child: Icon(Icons.info),
+                    child: Icon(
+                      Icons.info,
+                      size: 30,
+                    ),
                     onPressed: () {
                       Navigator.pushNamed(context, AppRoutes.info);
                     },
-                    backgroundColor: Colors.blue,
-                    elevation: 0.0),
-              ],
-            ))));
-  }
-}
-
-
-
-
-
-
-
-
-/*
-void main() {
-  runApp(const MyApp());
-}
-
-class StartPage extends StatelessWidget {
-  //const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Start',
-      home: MyStatefulWidget(),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: const AssetImage("assets/background.png"),
-                    fit: BoxFit.cover)),
-            child: Center(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text('Das kleine Gespenst im Museum',
-                    style: TextStyle(fontFamily: 'Pixellari')),
-                Image.asset('assets/images/ghost_right.png', scale: 0.2),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Starten',
-                    style: TextStyle(fontFamily: 'Pixellari'),
-                  ),
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.lime)),
-                ),
-                FloatingActionButton(
-                    child: Icon(Icons.info),
-                    onPressed: null,
                     backgroundColor: Colors.transparent,
                     elevation: 0.0),
               ],
             ))));
   }
 }
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Start',
-      home: MyStatefulWidget(),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            body: Container(
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: const AssetImage("assets/background.png"),
-                        fit: BoxFit.cover)),
-                child: Center(
-                    child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text('Das kleine Gespenst im Museum',
-                        style: TextStyle(fontFamily: 'Pixellari')),
-                    Image.asset('assets/images/ghost_right.png', scale: 0.2),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Starten',
-                        style: TextStyle(fontFamily: 'Pixellari'),
-                      ),
-                      style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.lime)),
-                    ),
-                    FloatingActionButton(
-                        child: Icon(Icons.info),
-                        onPressed: null,
-                        backgroundColor: Colors.transparent,
-                        elevation: 0.0),
-                  ],
-                )))));
-  }
-}*/
